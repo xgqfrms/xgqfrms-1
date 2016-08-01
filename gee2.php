b6ca260b0e27306b121502dab496898936df7070
xgqfrms <xgqfrms@outlook.com>
2016-08-01 14:11:03 +0800

    gee nodejs

---
+++ gee2.php
@@ -0,0 +1,21 @@
+<?php
+require_once dirname(dirname(__FILE__)) . '/lib/class.geetestlib.php';
+require_once dirname(dirname(__FILE__)) . '/config/config.php';
+session_start();
+$GtSdk = new GeetestLib(CAPTCHA_ID, PRIVATE_KEY);
+$user_id = $_SESSION['user_id'];
+if ($_SESSION['gtserver'] == 1) {
+    $result = $GtSdk->success_validate($_POST['geetest_challenge'], $_POST['geetest_validate'], $_POST['geetest_seccode'], $user_id);
+    if ($result) {
+        echo 'Yes!';
+    } else{
+        echo 'No';
+    }
+}else{
+    if ($GtSdk->fail_validate($_POST['geetest_challenge'],$_POST['geetest_validate'],$_POST['geetest_seccode'])) {
+        echo "yes";
+    }else{
+        echo "no";
+    }
+}
+?>
