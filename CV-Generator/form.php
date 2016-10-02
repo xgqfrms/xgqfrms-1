f73012663bb82091b738f06b94aa84b702930456
xgqfrms <xgqfrms@outlook.com>
2016-10-02 20:52:25 +0800

    ok

--- CV-Generator/form.php
+++ CV-Generator/form.php
@@ -9,7 +9,33 @@
 #echo "sm-title = {$_GET['sm-title']}";
 #echo "\n<br/>";
 #echo "get info";
-
+/*
+$_SERVER 
+# Predefined Variables 预定义变量
+## Superglobals  超级全局
+$GLOBALS
+$_​SERVER
+$_​GET
+$_​POST
+$_​FILES
+$_​REQUEST
+$_​SESSION
+$_​ENV
+$_​COOKIE
+$php_​errormsg
+$HTTP_​RAW_​POST_​DATA
+$http_​response_​header
+$argc
+$argv
+*/
+/*
+$globals = array(
+    '$_SERVER' => $_SERVER, '$_ENV' => $_ENV,
+    '$_REQUEST' => $_REQUEST, '$_GET' => $_GET,
+    '$_POST' => $_POST, '$_COOKIE' => $_COOKIE,
+    '$_FILES' => $_FILES, '$_CUSTOM' => $_CUSTOM
+);
+*/
  ?>
 <!DOCTYPE html>
 <html lang="en">
