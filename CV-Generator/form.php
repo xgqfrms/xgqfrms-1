166747814a1360c870527303b0463def44c7e10d
xgqfrms <xgqfrms@outlook.com>
2016-10-02 18:15:26 +0800

    PHP in HTML
    <span><?php echo "sm-name = {$_GET['sm-name']}"; ?></span>

--- CV-Generator/form.php
+++ CV-Generator/form.php
@@ -1,13 +1,15 @@
 <?php 
-// php $_GET 只能识别input 的name 属性，所以必须设置name 属性，以便PHP 对submit内容的进一步不处理！
-print_r($_GET);
-echo "\n";
-echo "\n<br/>";
-echo "sm-name = {$_GET['sm-name']} "; 
-echo "\n<br/>";
-echo "sm-title = {$_GET['sm-title']}";
-echo "\n<br/>";
-echo "get info";
+#php $_GET 只能识别input 的name 属性，所以必须设置name 属性，以便PHP 对submit内容的进一步不处理！
+
+#print_r($_GET);
+#echo "\n";
+#echo "\n<br/>";
+#echo "sm-name = {$_GET['sm-name']} "; 
+#echo "\n<br/>";
+#echo "sm-title = {$_GET['sm-title']}";
+#echo "\n<br/>";
+#echo "get info";
+
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +20,7 @@ echo "get info";
 <body>
 	<div>
 		<h1>form test</h1>
+		<span><?php echo "sm-name = {$_GET['sm-name']}"; ?></span>
 	</div>
 </body>
 </html>
