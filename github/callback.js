5bb6ad96248c444a7f0798a6d8410770771a07ea
xgqfrms <xgqfrms@outlook.com>
2016-12-11 09:56:52 +0800

    https://xgqfrms.xyz/github/postreceive
    //Webhooks: callback  
    
    
    
    https://www.xgqfrms.xyz/github/callback.js  
    
    https://xgqfrms.xyz/github/postreceive
    
    
    // https://developer.github.com/webhooks/  
    
    // https://developer.github.com/v3/repos/hooks/#get-single-hook  
    
    // ![GitHub high-level API-webhooks.png](GitHub high-level API-webhooks.png)

--- github/callback.js
+++ github/callback.js
@@ -1,1 +1,14 @@
-/Webhooks: callback/
+//Webhooks: callback  
+
+
+
+https://www.xgqfrms.xyz/github/callback.js  
+
+https://xgqfrms.xyz/github/postreceive
+
+
+// https://developer.github.com/webhooks/  
+
+// https://developer.github.com/v3/repos/hooks/#get-single-hook  
+
+// ![GitHub high-level API-webhooks.png](GitHub high-level API-webhooks.png)
